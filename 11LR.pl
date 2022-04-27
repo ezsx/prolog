fd290edab8457f17bdb116fecd5fe66fdeeadfc6
ezsx <scdcor@gmail.com>
2022-04-27 04:05:38 +0300

    lr11_task16

--- 11LR.pl
+++ 11LR.pl
@@ -60,11 +60,9 @@ grandMas(X):-parent(Z,X),parent(Y,Z),woman(Y),write(Y),nl.
 %14
 grandMaAndDa(X,Y):- parent(X,Z),parent(Z,Y),woman(Y),woman(X),write(yes),nl,fail;parent(Y,Z),parent(Z,X),woman(X),grandMa(Y,X),write(yes),nl,fail.
 grand_pa_and_son(X,Y):-parent(X,Z),parent(Z,Y),man(X),man(Y),write(yes),nl,fail;parent(Y,Z),parent(Z,X),man(Y),man(X),write(yes),nl,fail.
-
 in_list([],_):-fail.
 in_list([X|_],X).
 in_list([_|T],X):-in_list(T,X).
-
 %15
 minU(0,9):-!.
 minU(X,M):-
@@ -72,3 +70,7 @@ minU(X,M):-
 	minU(X1,M1),
 	M2 is X mod 10,
 	(M2<M1, M is M2;M is M1). 
+%16
+minD(X,M):- minD(X,9,M).
+minD(0,M,M):-!.
+minD(X,Y,M):-D is X mod 10, X1 is X div 10,D < Y,!,minD(X1,D,M); X2 is X div 10, minD(X2,Y,M).
