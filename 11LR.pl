e3fad8b61a00f8923663b6ecbb98f99243e5f8c6
ezsx <scdcor@gmail.com>
2022-04-27 04:11:32 +0300

    lr11_17

--- 11LR.pl
+++ 11LR.pl
@@ -74,3 +74,10 @@ minU(X,M):-
 minD(X,M):- minD(X,9,M).
 minD(0,M,M):-!.
 minD(X,Y,M):-D is X mod 10, X1 is X div 10,D < Y,!,minD(X1,D,M); X2 is X div 10, minD(X2,Y,M).
+%17	
+minOddU(0,99):-!.
+minOddU(X,M):-
+    X1 is X div 10,
+    minOddU(X1,M1),
+    M2 is X mod 10,
+    (M2 < M1, M2 mod 2 =\= 0 -> M is M2; M is M1).
