06b63e8931ca102f6cb737a932bda38ec360910e
ezsx <scdcor@gmail.com>
2022-06-04 10:14:58 +0300

    task1_1-5

---
+++ 14LR.pl
@@ -0,0 +1,32 @@
+
+% 1.1
+
+task1_1:- 
+    rS(Str,N), 
+    wS(Str), write(", "), 
+    wS(Str), write(", "), 
+    wS(Str), write(", "), 
+    write(N).
+
+rS(Str,N):- 
+    get0(Char), 
+    rS(Char, [], 0, Str, N, _).
+
+rS(Str,N, Flag):- 
+    get0(Char), 
+    rS(Char, [], 0, Str, N, Flag).
+
+rS(-1,Str,N,Str,N,1):-!.
+rS(10,Str,N,Str,N,0):-!.
+
+rS(Char,NowStr,Count,Str,N, Flag):-
+    NewCount is Count+1, 
+    appendString(NowStr,[Char],NewNowStr), 
+    get0(NewChar), 
+    rS(NewChar,NewNowStr,NewCount,Str,N, Flag).
+
+appendString([],X,X).
+appendString([X|T],Y,[X|T1]) :- appendString(T,Y,T1).
+
+wS([]):-!.
+wS([H|T]):-put(H),wS(T).
