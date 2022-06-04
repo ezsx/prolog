9a18923b53eaa449575b8cc89192d3eadc838be6
ezsx <scdcor@gmail.com>
2022-06-04 10:20:27 +0300

    1.3

--- 14LR.pl
+++ 14LR.pl
@@ -46,3 +46,52 @@ wordsCount([H|T], Count):-
 		Count is N+1;
 		wordsCount(T,Count).
 
+
+% 1.3 Дана строка, определить самое частое слово
+
+task1_3 :- readString(Str, _), mostCommW(Str, X), writeString(X).
+
+
+
+countWordsStr(List, X, Ans) :- 
+    countWordsStr(List, X, 0, Ans).
+countWordsStr([], _, Ans, Ans) :- !.
+countWordsStr([X|T], X, Count, Ans) :- 
+    NewCount is Count + 1, 
+    countWordsStr(T, X, NewCount, Ans),!.
+countWordsStr([_|T], X, Count, Ans) :- 
+    countWordsStr(T, X, Count, Ans),!.
+
+splitStr([], _, CurWord, CurWordList, Ans) :- 
+    appendString(CurWordList, [CurWord], NewWL), 
+    Ans = NewWL,!.
+
+splitStr([Separator|T], Separator, CurWord, CurWordList, Ans) :- 
+    appendString(CurWordList, [CurWord], NewWL), 
+    splitStr(T, Separator, [], NewWL, Ans),!.
+
+splitStr([S|T], Separator, CurWord, CurWordList, Ans) :- 
+    appendString(CurWord, [S], NewWord), 
+    splitStr(T, Separator, NewWord, CurWordList, Ans),!.
+
+splitStr(Str, Separator, Ans) :- 
+    char_code(Separator, SepCode), 
+    splitStr(Str, SepCode, [], [], Ans).
+
+
+
+mostCommW(Str, Ans) :-
+    splitStr(Str, " ", Words), 
+    mostCommW(Words, Words, 0, [], Ans). 
+
+mostCommW(Words, [Word|T], CurMaxCnt, _, Ans) :- 
+    countWordsStr(Words, Word, Cnt), 
+    Cnt > CurMaxCnt, 
+    NewMax is Cnt, 
+    NewMaxWord = Word, 
+    mostCommW(Words, T, NewMax, NewMaxWord, Ans),!.
+
+mostCommW(Words, [_|T], CurMaxCnt, CurMaxWord, Ans) :- 
+    mostCommW(Words, T, CurMaxCnt, CurMaxWord, Ans),!.
+
+mostCommW(_, [], _, Ans, Ans) :-!.
